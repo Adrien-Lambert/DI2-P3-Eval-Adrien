f6b90fdf30a64db815864ca44a8d62c8dde1c823
Adrien LAMBERT <adrien.lambert@diiage.org>
2025-02-26 14:04:06 +0100

    Front - Updated api url to use the right port

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7254/api',
+    apiUrl: 'https://localhost:7283/api',
     apiKey :'66b5bb4b-73b5-405f-a7dd-09c5f821efe3'
   };
