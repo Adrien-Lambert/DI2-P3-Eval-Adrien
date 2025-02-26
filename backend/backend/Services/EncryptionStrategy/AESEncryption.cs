56fea31ecc4d8c80e394c2b3d7216c57b2b1c04e
Adrien LAMBERT <adrien.lambert@diiage.org>
2025-02-26 11:40:28 +0100

    Added AES encryption

---
+++ backend/backend/Services/EncryptionStrategy/AESEncryption.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace backend.Services.EncryptionStrategy
+{
+    public class AESEncryption : IEncryptionStrategy
+    {
+        private readonly byte[] Key = Encoding.UTF8.GetBytes("ThisIsASecretKey123");
+        private readonly byte[] IV = Encoding.UTF8.GetBytes("ThisIsAnIV123456");
+
+        public string Encrypt(string plainText)
+        {
+            using Aes aes = Aes.Create();
+            aes.Key = Key;
+            aes.IV = IV;
+            using var encryptor = aes.CreateEncryptor();
+            byte[] inputBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[] encryptedBytes = encryptor.TransformFinalBlock(inputBytes, 0, inputBytes.Length);
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            using Aes aes = Aes.Create();
+            aes.Key = Key;
+            aes.IV = IV;
+            using var decryptor = aes.CreateDecryptor();
+            byte[] inputBytes = Convert.FromBase64String(cipherText);
+            byte[] decryptedBytes = decryptor.TransformFinalBlock(inputBytes, 0, inputBytes.Length);
+            return Encoding.UTF8.GetString(decryptedBytes);
+        }
+    }
+}
