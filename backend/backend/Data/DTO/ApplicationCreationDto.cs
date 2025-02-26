fd4c2a409e9f8aad844222e0a87664e87051cd8d
Adrien LAMBERT <adrien.lambert@diiage.org>
2025-02-26 11:39:55 +0100

    Added application DTOs

---
+++ backend/backend/Data/DTO/ApplicationCreationDto.cs
@@ -0,0 +1,18 @@
+﻿using backend.Logic.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace backend.Data.DTO
+{
+    public class ApplicationCreationDto
+    {
+        [Required]
+        [MaxLength(50)]
+        [JsonPropertyName("application_name")]
+        public string ApplicationName { get; set; }
+
+        [Required]
+        [JsonPropertyName("application_type")]
+        public ApplicationType ApplicationType { get; set; }
+    }
+}
---
+++ backend/backend/Data/DTO/ApplicationReadDto.cs
@@ -0,0 +1,19 @@
+﻿using backend.Logic.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace backend.Data.DTO
+{
+    public class ApplicationReadDto
+    {
+        [JsonPropertyName("application_id")]
+        public int ApplicationId { get; set; }
+
+        [JsonPropertyName("application_name")]
+        public string ApplicationName { get; set; }
+
+        [Required]
+        [JsonPropertyName("application_type")]
+        public ApplicationType ApplicationType { get; set; }
+    }
+}
